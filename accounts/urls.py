158b688a2c9846a82e10873793d3d7b2e6a957d7
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-04-16 16:48:22 +0530

    Implemented login and logout of normal users

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     url(r"^signup/$", views.signup, name = "signup"),
-    #url(r"^login/$", views.login, name = "login")
+    url(r"^login/$", views.login, name = "login"),
+    url(r"^logout/$", views.logout_view, name = "logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as logi
+from django.contrib.auth import logout
 
 def signup(request):
     if request.method == "POST":
@@ -7,8 +9,9 @@ def signup(request):
         UCF = UserCreationForm(request.POST)
         if UCF.is_valid():
             UCF.save()
-            # TODO: log the user in
-            return redirect("articles:list")
+
+            # redirect to login page so that the user might enter their credentials to log in
+            return redirect("accounts:login") 
 
     else: 
         # create a blank instance of UserCreationForm
@@ -16,3 +19,29 @@ def signup(request):
 
     # pass on this instance in the render call
     return render(request, "accounts/signup.html", {"form": UCF})
+
+def login(request):
+    if request.method == "POST":
+        # create a form with the user data
+        loginForm = AuthenticationForm(data = request.POST)
+
+        if loginForm.is_valid():
+            # retrieve the user from the credentials entered in the login form
+            user = loginForm.get_user()
+            # log this user in
+            logi(request, user)
+
+            return redirect("articles:list") 
+    
+    else:
+        # create a blank instance of login form
+        loginForm = AuthenticationForm()
+    
+    # pass on the latest instance (GET / unvalidated POST) to the template
+    return render(request, "accounts/login.html", {"form": loginForm})
+
+def logout_view(request):
+    # Logout the user if he hits the logout submit button
+    if request.method == "POST":
+        logout(request)
+        return redirect("accounts:login")
