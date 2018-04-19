dbbcf73dbaedb5b330d1fbcd47f58fa0c9ba4f77
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-04-19 15:20:38 +0530

    Added the 'require login' feature

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,6 @@ app_name = "articles" # name-spacing this app
 
 urlpatterns = [
     url (r"^$", views.article_list, name="list"),
+    url (r"^create/$", views.article_create, name="create"),
     url (r"^(?P<slug>[\w-]+)/$", views.article_detail, name="detail")
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article # from models.py module in the same folder, import the Article class
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def article_list (request):
     # obtain all the instances of the Article class, and order them by their 'date' attributes (newest first)
@@ -22,4 +23,9 @@ def article_detail (request, slug):
 
     # allow this dictionary to be accessible to articles.html so that articles.html can use the article and render them on the webpage
     return render(request, "articles/article_detail.html", dici)
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    # login required to add new articles
+    return render(request, "articles/article_create.html")
 
